749bb7d10aff59cdefe241eac4d65e6521754f48
DeVoresyah Arest <de.voresyah@gmail.com>
2025-09-17 17:59:48 +0700

    fix: add metadata on adonis commands

--- commands/index.ts
+++ commands/index.ts
@@ -1,6 +1,47 @@
 /**
- * Export all commands for Adonis Open Swagger
+ * Adonis Open Swagger Commands
  */
 
-export { default as GenerateDocs } from './generate_docs.js'
-export { default as ValidateSpec } from './validate_spec.js'
+import type { CommandMetaData } from '@adonisjs/ace/types'
+import type { BaseCommand } from '@adonisjs/core/ace'
+import GenerateDocs from './generate_docs.js'
+import ValidateSpec from './validate_spec.js'
+
+/**
+ * Returns an array of command metadata
+ */
+export async function getMetaData(): Promise<CommandMetaData[]> {
+  return [
+    {
+      commandName: GenerateDocs.commandName,
+      description: GenerateDocs.description,
+      namespace: null,
+      aliases: GenerateDocs.aliases || [],
+      flags: [],
+      args: [],
+      options: GenerateDocs.options || {},
+    },
+    {
+      commandName: ValidateSpec.commandName,
+      description: ValidateSpec.description,
+      namespace: null,
+      aliases: ValidateSpec.aliases || [],
+      flags: [],
+      args: [],
+      options: ValidateSpec.options || {},
+    },
+  ]
+}
+
+/**
+ * Returns the command class for the given metadata
+ */
+export async function getCommand(metaData: CommandMetaData): Promise<typeof BaseCommand | null> {
+  if (metaData.commandName === GenerateDocs.commandName) {
+    return GenerateDocs
+  }
+  if (metaData.commandName === ValidateSpec.commandName) {
+    return ValidateSpec
+  }
+  return null
+}
